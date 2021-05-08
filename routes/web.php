e1571f687ce980bf3acc65e217b339cf64082653
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-05-07 23:01:35 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,11 @@ Rotas da aplicação. Tem conexão direta com a pasta resources/views.
 Na pasta Public foi criado as pastas css, img e js.
 */
 
-Route::get('/', function () { // Rota da página principal, "Home" //
-    return view('home');
-});
+use App\Http\Controllers\EventoController;
+
+Route::get('/', [EventoController::class, 'index']); // Rota Controller da página Home //
+Route::get('/eventos/create', [EventoController::class, 'create']);
+
 
 Route::get('/contato', function () { // Rota da página de contatos //
     return view('contato');
