d5af70e2b7bd405b86a9ace2164265f3e268edcc
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-05-07 23:00:51 -0300

    Create create.blade.php

---
+++ resources/views/eventos/create.blade.php
@@ -0,0 +1,9 @@
+@extends('template'){{--Extenção do arquivo template--}}
+
+@section('title', 'Criar Evento'){{--Extenção do arquivo template--}}
+
+@section('content'){{--Extenção do arquivo template--}}
+
+<h1 class="titulo">Criar um evento</h1>
+
+@endsection {{--Extenção do arquivo template--}}
