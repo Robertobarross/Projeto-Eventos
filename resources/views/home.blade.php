aad9995c5800aa661e425b810e4e2243053f7830
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-05-07 23:01:05 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,11 @@
 
 @section('content'){{--Extenção do arquivo template--}}
 
-        <h1 class="titulo">Home aula 9</h1>
+        <h1 class="titulo">Home aula 14</h1>
         <img src="img/img-event.jpg" alt="banner">
+
+        @foreach ($eventos as $evento)
+        <p>{{ $evento->titulo }} -- {{ $evento->descricao }} -- {{ $evento->cidade }}</p>{{--Declarando variáveis na página home--}}
+        @endforeach
 
 @endsection {{--Extenção do arquivo template--}}
