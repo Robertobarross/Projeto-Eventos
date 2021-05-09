0ff73a6654ec31567664c8d8d25cb9579b88fc62
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-05-08 21:59:08 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,8 @@ Na pasta Public foi criado as pastas css, img e js.
 use App\Http\Controllers\EventoController;
 
 Route::get('/', [EventoController::class, 'index']); // Rota Controller da página Home //
-Route::get('/eventos/create', [EventoController::class, 'create']);
-
+Route::get('/eventos/create', [EventoController::class, 'create']); // Rota Controller criar eventos
+Route::post('/eventos', [EventoController::class, 'store']);
 
 Route::get('/contato', function () { // Rota da página de contatos //
     return view('contato');
