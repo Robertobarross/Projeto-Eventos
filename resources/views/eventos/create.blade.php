1acd1a5ce61d291aa3bf9e5cfc60385d62a922f8
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-05-08 21:58:53 -0300

    Update create.blade.php

--- resources/views/eventos/create.blade.php
+++ resources/views/eventos/create.blade.php
@@ -4,6 +4,40 @@
 
 @section('content'){{--Extenção do arquivo template--}}
 
-<h1 class="titulo">Criar um evento</h1>
+<div id="evento-create-container" class="col-md-6.offset-md-3">
+    <h1>Crie seu evento</h1>
+    <form action="/eventos" method="post">
+    @csrf
+
+    <div class="form-group">
+      <label for="title">Evento</label>
+      <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Nome do evento">
+    </div>
+
+    <div class="form-group">
+        <label for="title">Cidade</label>
+        <input type="text" class="form-control" id="cidade" name="cidade" placeholder="Nome da cidade">
+      </div>
+
+      <div class="form-group">
+        <label for="title">Descrição</label>
+        <textarea name="descrição" id="descrição" class="form-control" placeholder="Informações sobre o evento?"></textarea>
+      </div>
+
+      <div class="form-group">
+        <label for="title">O evento é privado?</label>
+        <select name="privado" id="privado" class="form-control">
+        <option value="0">Não</option>
+        <option value="1">Sim</option>
+        </select>
+      </div>
+
+      <input type="Submit" class="btn btn-primary" value="Criar Evento">
+
+    </form>
+</div>
+
+
+
 
 @endsection {{--Extenção do arquivo template--}}
