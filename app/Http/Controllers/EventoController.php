931c7d763c383e524dd720d626f3f94c8535f7d7
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-05-08 21:58:39 -0300

    Update EventoController.php

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -8,11 +8,26 @@ use App\Models\Evento; // Incluindo Model Evento //
 class EventoController extends Controller
 {
     public function index(){ // Rota da página principal, "Home" //
-        $eventos = Evento::all(); // Pegando arquivos do bd //
+        $eventos = Evento::all(); // Pegando arquivos do bd, tabela eventos//
         return view('home', ['eventos' => $eventos]); // Desclando variáveis //
 }
 
-    public function create(){
+    public function create(){ // Rota da página criar eventos //
         return view('eventos.create');
+    }
+
+
+    public function store(Request $request){ // O arquivo a baixo  é responsável por enviar as informações para o bd, e públicar na página de eventos //
+        $evento = new Evento;
+
+        $evento->titulo = $request->titulo;
+        $evento->cidade = $request->cidade;
+        $evento->privado = $request->privado;
+        $evento->descricao = $request->descricao;
+
+        $evento->save();
+
+      //  return redirect('/create');
+
     }
 }
