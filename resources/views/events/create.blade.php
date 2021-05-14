c3dace07b2bfb71c0ac560cdfe197300af157358
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-05-13 21:55:35 -0300

    Update create.blade.php

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -10,6 +10,11 @@
     @csrf
 
     <div class="form-group">
+        <label for="image">Imagem do Evento</label>
+        <input type="file" class="form-control" id="image" name="image">
+    </div>
+
+    <div class="form-group">
       <label for="title">Evento</label>
       <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Nome do evento">
     </div>
@@ -21,7 +26,7 @@
 
       <div class="form-group">
         <label for="title">Descrição</label>
-        <textarea name="descrição" id="descrição" class="form-control" placeholder="Informações sobre o evento?"></textarea>
+        <textarea name="informe" id="informe" class="form-control" placeholder="Informações sobre o evento?"></textarea>
       </div>
 
       <div class="form-group">
