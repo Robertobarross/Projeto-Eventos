fcb84f3ee79f285a4f0dead8dbac6ff86d4226c5
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-05-15 22:33:47 -0300

    Update Event.php

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    protected $casts = [ // É necessário informar que o campo intens é um array //
+        'itens' => 'array'];
+
+    protected $dates = ['date']; // É necessário informar que o campo data foi criado //
+
+    public function user(){
+        return $this->belongsTo('App\Models\user');
+    }
 }
