1100de3d4befd069118d43987e6a9cc7b195880b
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-05-19 20:01:49 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,22 @@ use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']); // Rota Controller da página Home //
 Route::get('/events/create', [EventController::class, 'create'])->middleware('auth'); // Rota Controller criar eventos //
-Route::get('/events/{id}', [EventController::class, 'show']); // Rota Controller show //
-Route::post('/events', [EventController::class, 'store']); // Rota Controller das postagens //
+Route::get('/events/{id}', [EventController::class, 'show'])->middleware('auth'); // Rota Controller show //
+Route::post('/events', [EventController::class, 'store'])->middleware('auth'); // Rota Controller das postagens //
+Route::delete('/events/{id}', [EventController::class, 'destroy'])->middleware('auth');// Rota delete //
+Route::get('/events/edit/{id}', [EventController::class, 'edit'])->middleware('auth');// Rota de Editar //
+Route::put('/events/update/{id}', [EventController::class, 'update'])->middleware('auth'); // Para fazer a atualização da edição //
+
+
 
 Route::get('/contato', function () { // Rota da página de contatos //
         return view('contato');
     });
 
+Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth'); // Rota Dashboard //
+
+
+/*
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+    return view('dashboard'); // Rota do login //
+})->name('dashboard');*/
