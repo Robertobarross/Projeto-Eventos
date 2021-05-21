d6593f18b2a41edaa244052393e41d849dced652
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-05-20 22:35:24 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,14 @@ Route::put('/events/update/{id}', [EventController::class, 'update'])->middlewar
 
 Route::get('/contato', function () { // Rota da página de contatos //
         return view('contato');
-    });
+    })->middleware('auth');
 
 Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth'); // Rota Dashboard //
 
 Route::post('/events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth'); // Rota event_user confirmar presença no evento //
+
+Route::delete('/events/leave/{id}', [EventController::class, 'leaveEvent'])->middleware('auth'); // Rota sair do evento //
+
 
 /*
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
